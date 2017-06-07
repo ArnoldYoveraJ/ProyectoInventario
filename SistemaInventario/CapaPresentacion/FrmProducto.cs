f2034a10b2f155bda9707899e1d99905eb37fe32
Arnold Yovera Jibaja <arnoldyj_6_92@hotmail.com>
2017-06-07 16:53:09 -0500

    Actualizacion de los botones Eliminar, editar y otros.

--- SistemaInventario/CapaPresentacion/FrmProducto.cs
+++ SistemaInventario/CapaPresentacion/FrmProducto.cs
@@ -25,7 +25,7 @@ namespace CapaPresentacion
             this.ttmensaje.SetToolTip(this.cboestado, "Seleccione el estado del Producto");
             this.txtcod_pro.Visible = false;
             this.txttrabajador.ReadOnly = true;
-            this.LlenarComboCategoria();
+            this.Llenar_Combo_Categoria();
         }
         //Mostrar Mensaje de Confirmacion
         private void MensajeOK(string mensaje)
@@ -133,7 +133,7 @@ namespace CapaPresentacion
             this.IsEditar = false;
         }
 
-        private void LlenarComboCategoria()
+        private void Llenar_Combo_Categoria()
         {
             cbocategoria.DataSource = NCategoria.Mostrar();
             cbocategoria.ValueMember = "cod_cat";
