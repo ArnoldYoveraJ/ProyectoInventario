71f026eb90d5858aba2a4cc58fe803e99dcdeb0d
Arnold Yovera Jibaja <arnoldyj_6_92@hotmail.com>
2017-06-07 17:24:51 -0500

    Actualizacion botones Eliminar, editar.

--- SistemaInventario/CapaPresentacion/FrmProducto.cs
+++ SistemaInventario/CapaPresentacion/FrmProducto.cs
@@ -25,7 +25,7 @@ namespace CapaPresentacion
             this.ttmensaje.SetToolTip(this.cboestado, "Seleccione el estado del Producto");
             this.txtcod_pro.Visible = false;
             this.txttrabajador.ReadOnly = true;
-            this.LlenarCombo_Categoria();
+            this.LlenarComboCategoria();
         }
         //Mostrar Mensaje de Confirmacion
         private void MensajeOK(string mensaje)
@@ -133,7 +133,7 @@ namespace CapaPresentacion
             this.IsEditar = false;
         }
 
-        private void LlenarCombo_Categoria()
+        private void LlenarComboCategoria()
         {
             cbocategoria.DataSource = NCategoria.Mostrar();
             cbocategoria.ValueMember = "cod_cat";
