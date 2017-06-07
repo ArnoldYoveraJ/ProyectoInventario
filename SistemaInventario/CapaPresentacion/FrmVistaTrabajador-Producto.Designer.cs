bf00e4fdb302fa2b46706c878cc22aaedfff6227
Arnold Yovera Jibaja <arnoldyj_6_92@hotmail.com>
2017-06-07 17:25:36 -0500

    aaaa

---
+++ SistemaInventario/CapaPresentacion/FrmVistaTrabajador-Producto.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CapaPresentacion
+{
+    partial class FrmVistaTrabajador_Producto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ttMensaje = new System.Windows.Forms.ToolTip(this.components);
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgvlistado = new System.Windows.Forms.DataGridView();
+            this.Eliminar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.erroricono = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvlistado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erroricono)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ttMensaje
+            // 
+            this.ttMensaje.IsBalloon = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgvlistado);
+            this.tabPage1.Controls.Add(this.txtbuscar);
+            this.tabPage1.Controls.Add(this.btnbuscar);
+            this.tabPage1.Controls.Add(this.lbltotal);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(471, 238);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Listado";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgvlistado
+            // 
+            this.dgvlistado.AllowUserToAddRows = false;
+            this.dgvlistado.AllowUserToDeleteRows = false;
+            this.dgvlistado.AllowUserToOrderColumns = true;
+            this.dgvlistado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvlistado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Eliminar});
+            this.dgvlistado.Location = new System.Drawing.Point(19, 85);
+            this.dgvlistado.MultiSelect = false;
+            this.dgvlistado.Name = "dgvlistado";
+            this.dgvlistado.ReadOnly = true;
+            this.dgvlistado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvlistado.Size = new System.Drawing.Size(428, 134);
+            this.dgvlistado.TabIndex = 7;
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.HeaderText = "Eliminar";
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.ReadOnly = true;
+            this.Eliminar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Eliminar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Location = new System.Drawing.Point(66, 27);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(132, 20);
+            this.txtbuscar.TabIndex = 6;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(207, 24);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnbuscar.TabIndex = 3;
+            this.btnbuscar.Text = "&Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(400, 63);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(39, 13);
+            this.lbltotal.TabIndex = 2;
+            this.lbltotal.Text = "Label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Trabajadores";
+            // 
+            // erroricono
+            // 
+            this.erroricono.ContainerControl = this;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(39, 74);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(479, 264);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // FrmVistaTrabajador_Producto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 381);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmVistaTrabajador_Producto";
+            this.Text = "Lista de Trabajadores";
+            this.Load += new System.EventHandler(this.FrmVistaTrabajador_Producto_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvlistado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erroricono)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip ttMensaje;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dgvlistado;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Eliminar;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ErrorProvider erroricono;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
---
+++ SistemaInventario/CapaPresentacion/FrmVistaTrabajador-Producto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class FrmVistaTrabajador_Producto : Form
+    {
+        public FrmVistaTrabajador_Producto()
+        {
+            InitializeComponent();
+        }
+        //ocultar columnas
+        private void ocultarcolumnas()
+        {
+            this.dgvlistado.Columns[0].Visible = false;
+            this.dgvlistado.Columns[1].Visible = false;
+        }
+
+        //Mostrar
+        private void mostrar()
+        {
+            this.dgvlistado.DataSource = NCategoria.Mostrar();
+            this.ocultarcolumnas();
+            lbltotal.Text = "Total de Registros: " + Convert.ToString(dgvlistado.Rows.Count);
+        }
+
+        //Buscar Categoria por nombre
+        private void buscar_nombre()
+        {
+            this.dgvlistado.DataSource = NCategoria.BuscarNombre(this.txtbuscar.Text);
+            this.ocultarcolumnas();
+            lbltotal.Text = "Total de Registros: " + Convert.ToString(dgvlistado.Rows.Count);
+        }
+        private void FrmVistaTrabajador_Producto_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
